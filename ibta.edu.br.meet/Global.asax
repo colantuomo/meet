d36f6c946fca0547b1f6d8efea4d22d7e729ca62
colantuomo <pcolantuomo@gmail.com>
2017-11-12 16:02:38 -0200

    up to git

---
+++ ibta.edu.br.meet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ibta.edu.br.meet.MvcApplication" Language="C#" %>
---
+++ ibta.edu.br.meet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ibta.edu.br.meet.Startup))]
+namespace ibta.edu.br.meet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
