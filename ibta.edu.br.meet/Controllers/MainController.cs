ca9b06e6eb53bf6f848ec250a59a57f0db380396
colantuomo <pcolantuomo@gmail.com>
2017-11-12 18:59:43 -0200

    Criado card

---
+++ ibta.edu.br.meet/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ibta.edu.br.meet.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult TelaPrincipal()
+        {
+            return View();
+        }
+    }
+}
