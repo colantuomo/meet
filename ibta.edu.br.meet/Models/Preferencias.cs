0bb7c77c62968b7da2e8256a0908974e65ca1c56
colantuomo <pcolantuomo@gmail.com>
2017-11-25 22:46:11 -0200

    Criando table e model de preferencias

---
+++ ibta.edu.br.meet/Models/Preferencias.cs
@@ -0,0 +1,18 @@
+namespace ibta.edu.br.meet.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Preferencias
+    {
+        [Key]
+        public int IdPreferencia { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Descricao { get; set; }
+    }
+}
---
+++ ibta.edu.br.meet/Models/PreferenciasModel.cs
@@ -0,0 +1,21 @@
+namespace ibta.edu.br.meet.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=PreferenciasModel")
+        {
+        }
+
+        public virtual DbSet<Preferencias> Preferencias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
